4d3ad3969d6f44ebb8d3ecf5daa6db582932b6b6
Victor Meza <vicalfmg1109@gmail.com>
2021-12-24 13:09:06 -0300

    Practicando DOM JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+/*Practicando con el DOM */ 
+
+var btn = document.getElementById("section-btn");
+
+btn.addEventListener("click" , function(e) {
+    e.preventDefault();
+    alert("Hola mundo");
+});
+
+var innerWidth = window.innerWidth;
+
+if ( innerWidth < 750) {
+    var nav = document.getElementById("nav-id");
+    nav.style.display= "none";
+
+}
 
